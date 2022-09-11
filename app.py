2119378cd1292902060a14da707d93287de3abf2
Josh Norman <j.norm911@gmail.com>
2022-09-11 13:15:55 -0700

    Added Pandas back in

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from readline import append_history_file
 from user import getWallets, selectWallet, walletString
 
 getWallets()
 selectWallet()
 print("You have selected wallet " + walletString())
+
